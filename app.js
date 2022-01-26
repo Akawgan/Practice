b7981d99573a2182a6a9bf5bf505a5ce20c7daa1
Savien <akawgan@gmail.com>
2022-01-26 21:43:11 +0100

    Add three more buttons for various animation operations

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 function test()
 {
   let picture = document.getElementById("target");
+  picture.offsetHeight;
+  picture.className = "animate";
+  console.log("Launched");
+}
+function test2()
+{
+  let picture = document.getElementById("target");
+  picture.style.display = "none";
+  console.log("Launched");
+}
+function test3()
+{
+  picture.style.display = "flex";
+  console.log("Launched");
+}
+function test4()
+{
+  let picture = document.getElementById("target");
+  picture.style.display = "none";
   picture.className = "animate";
   console.log("Launched");
 }
